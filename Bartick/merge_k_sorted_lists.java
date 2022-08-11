20b59e41dfd2ec0959659d1eeaa687fc8f3cb088
Bartick Maiti <69100224+bartick@users.noreply.github.com>
2022-08-11 22:48:20 +0530

    Create merge_k_sorted_lists.java

---
+++ Bartick/merge_k_sorted_lists.java
@@ -0,0 +1,56 @@
+// https://leetcode.com/problems/merge-k-sorted-lists/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        return divideAndConqure(lists, 0, lists.length - 1);
+    }
+
+    private ListNode divideAndConqure(ListNode[] lists, int start, int end) {
+        if (start == end) {
+            return lists[start];
+        }
+        int mid = start + (end - start) / 2;
+        ListNode left = divideAndConqure(lists, start, mid);
+        ListNode right = divideAndConqure(lists, mid + 1, end);
+        return mergeSort(left, right);
+    }
+
+    private ListNode mergeSort(ListNode left, ListNode right) {
+        ListNode head = new ListNode(-1);
+        ListNode temp = head;
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                temp.next = left;
+                left = left.next;
+            } else {
+                temp.next = right;
+                right = right.next;
+            }
+            temp = temp.next;
+        }
+        while (left != null) {
+            temp.next = left;
+            left = left.next;
+            temp = temp.next;
+        }
+        while (right != null) {
+            temp.next = right;
+            right = right.next;
+            temp = temp.next;
+        }
+        return head.next;
+    }
+}
