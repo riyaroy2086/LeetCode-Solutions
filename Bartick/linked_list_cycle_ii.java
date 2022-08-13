13eb9083e927787f2c2e7049e4414d2c5b6d4660
Bartick Maiti <69100224+bartick@users.noreply.github.com>
2022-08-13 23:53:33 +0530

    Create linked_list_cycle_ii.java

---
+++ Bartick/linked_list_cycle_ii.java
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/linked-list-cycle-ii/
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) break;
+        }
+        if (fast == null || fast.next == null) return null;
+        while (head != slow) {
+            head = head.next;
+            slow = slow.next;
+        }
+        return head;
+    }
+}
