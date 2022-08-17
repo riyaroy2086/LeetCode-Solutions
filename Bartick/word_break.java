10e34e4f491490026401bd64c2fc9a5be3e9a4aa
Bartick Maiti <69100224+bartick@users.noreply.github.com>
2022-08-17 23:55:43 +0530

    Create word_break.java

---
+++ Bartick/word_break.java
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/word-break/
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] isWordBreak = new boolean[s.length() + 1];
+        
+        isWordBreak[0] = true;
+        
+        for (int i = 0; i < s.length() + 1; i++) {
+            for (int j = 0; j < i; j++) {
+                if (!isWordBreak[j]) {
+                    continue;
+                }
+                
+                if (wordDict.contains(s.substring(j, i))) {
+                    isWordBreak[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        return isWordBreak[s.length()];
+    }
+}
