0d0187bc2c463b0a12b413c4331c92fe887a74bc
Bartick Maiti <69100224+bartick@users.noreply.github.com>
2022-09-10 23:32:44 +0530

    Create all_elements_in_two_binary_search_tree.java

---
+++ Bartick/all_elements_in_two_binary_search_tree.java
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/all-elements-in-two-binary-search-trees/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> list;
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        list = new ArrayList<Integer>();
+        dfs(root1);
+        dfs(root2);
+        Collections.sort(list);
+        return list;
+    }
+    
+    public void dfs(TreeNode root) {
+        if (root == null) return;
+        dfs(root.left);
+        list.add(root.val);
+        dfs(root.right);
+    }
+}
